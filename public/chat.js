25cc3c56e0c5bd8e918273ee229c5ecbd5ef1ad2
Lacy Thomas <lacy.thomas12@gmail.com>
2014-06-24 19:21:05 -0400

    committing beginning mongo/node mongo driver files

--- public/chat.js
+++ public/chat.js
@@ -8,11 +8,10 @@ window.onload = function() {
     var name = $("#name");
  
     socket.on('message', function (data) {
-        if(data.message) {
+        if (data.message) {
             messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += '<img src="https://teksyndicate.com/sites/teksyndicate.com/files/profiles/not%20yet%20assigned/9323a93a0b591c8643e444acf8ea5aea.jpg" style="width:20px; height:20px;" />';
                 html += '<b>' + (messages[i].username || 'Server') + ': </b>';
                 html += messages[i].message + '<br />';
             }
--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var client = require('mongodb').MongoClient;
 var app = express();
 var port = 3700;
  
 app.set('views', __dirname + '/tpl');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
 
-app.get("/", function(req, res){
-    res.render("page.html");
+app.post("/user", function(req, res) {
+	client.connect("mongodb://localhost:27017/test", function(err, db) {
+		db.createCollection("user", function(err, users){
+			//TODO save cookies, get fat/username.
+    		users.insert({ "username": req.body.name, token: "lacy's cookie goes here." }, function(err) {
+    		});
+		});
+	});
+
+	res.render("chat.html");
 });
 
-app.use(express.static(__dirname + '/public'));
+app.get("/", function(req,res) {
+	res.render("index.html");
+});
+
+app.get("/chat", function(req, res){
+    res.render("chat.html");
+});
 
 var io = require('socket.io').listen(app.listen(port));
 
