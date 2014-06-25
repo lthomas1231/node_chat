8920e5e8c7f2096907cdb03561b692a423118ee5
Lacy Thomas <lacy.thomas12@gmail.com>
2014-06-24 20:33:24 -0400

    Add cookie parser and begin generating token

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var express = require("express");
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var client = require('mongodb').MongoClient;
 var app = express();
 var port = 3700;
+var crypto = require('crypto');
+
  
 app.set('views', __dirname + '/tpl');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', "ejs");
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
+app.use(cookieParser());
 
 app.post("/user", function(req, res) {
+	var token = newToken();
+	res.cookie('cookieName', token);
+
 	client.connect("mongodb://localhost:27017/test", function(err, db) {
-		db.createCollection("user", function(err, users){
-			//TODO save cookies, get fat/username.
-    		users.insert({ "username": req.body.name, token: "lacy's cookie goes here." }, function(err) {
+		db.createCollection("user", function(err, users) {
+    		users.insert({ "username": req.body.name, token: token }, function(err) {
+
     		});
 		});
 	});
@@ -27,6 +35,7 @@ app.get("/", function(req,res) {
 });
 
 app.get("/chat", function(req, res){
+	console.log(req.cookies);
     res.render("chat.html");
 });
 
@@ -38,5 +47,16 @@ io.sockets.on('connection', function (socket) {
 		io.sockets.emit('message', data);
 	});
 });
+
+//TODO figure out how to make this function wait for token to be populated
+var newToken = function() {
+	var token;
+	crypto.randomBytes(48, function(ex, buf) {
+		console.log(ex, buf, buf.toString('hex'));
+		token = buf.toString('hex');
+	});
+
+	return token;
+};
 
 console.log("Listening on port " + port);
